6d05b1838ac139e0f06109d362516220a082b726
unknown <ferreiraeduardo822@gmail.com>
2022-04-07 21:46:00 -0300

    add: estilos-ui

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import { makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import  Toolbar from '@material-ui/core/Toolbar';
+const useStyles = makeStyles({
+    appBar:{
+        boxShadow: 'none',
+    }
+});
 
 function Header(){
+    const classes = useStyles();
     return(
-        <header className='header'>
-            <div className='toolbar'>
-                <div >
-                    <span>Conecta Dev</span>
+        <AppBar position='fixed' color='inherit' className={classes.appBar}>
+            <Toolbar>
+                <img src="/images/logo.png" alt="logo"/>
+                {/*<div>
+                    <a href="/">Conecta Dev</a>
+                    <input type="text"></input>
                 </div>
                 <div >
-                <button>Novo Post</button>
+                    <Button color= 'primary' variant="contained">
+                        Novo Post
+                    </Button>
                         <span>img1</span>
                         <span>img2</span>
-                </div>
-            </div>
-        </header>
+                </div>*/}
+            </Toolbar>
+        </AppBar>
     );
 }
 
